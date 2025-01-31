d28f00f5e2afc0817041e181483ef1fc5cf41ac4
Dmytro Zhuk <grandatlant@gmail.com>
2025-01-31 21:14:06 +0200

    Update remove-unused-sv.py
    Changed WOW_PATH to upped dir ../

--- remove-unused-sv.py
+++ remove-unused-sv.py
@@ -11,7 +11,7 @@ if os.name == "posix":
 elif (os.name == "win") or (os.name == "nt"):
     WOW_PATH = r"E:\World of Warcraft\\"
 
-WOW_PATH = './'
+WOW_PATH = '../'
 
 def listfiles(path):
     return [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
