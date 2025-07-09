bb0d5e188cbe319d0e3e77eee3f67e4f53203887
grandatlant <grandatlant@gmail.com>
2025-07-09 03:13:55 +0300

    update for pack_patch_mpq.py script and readme

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Data
 'Data' directory for World of Warcraft custom Data changes such as Sounds or Fonts.
 
-Merge this files into Data directory to get profit:
+Merge this files into Data directory (or just patch-Y.MPQ file alone) to get profit:
 
-- realmlist.wtf for warmane.com
 - Creature/ replacement for Ghosts (Lady Deathwhisper ICC fight)
 - Fonts/ for cyrillic charset
 - Sound/ replacement for annoying sounds e.g. Goblin Weather Machine, Argent Gruntling/Squire, Lament of the Highborne song, Fizzle spell sounds, Character voice emotions
-- [script](https://github.com/grandatlant/DataMod/blob/main/pack_patch_mpq.py) for packing all files from Creature/, Fonts/, Sound/ to one patch-Y.MPQ
-- [patch-Y.MPQ](https://github.com/grandatlant/DataMod/blob/main/patch-Y.MPQ) itself with all contents
+- [patch-Y.MPQ](https://github.com/grandatlant/DataMod/blob/main/patch-Y.MPQ) itself with all contents above
+
+Additional content besides patch:
+- 'realmlist.wtf' for warmane.com private server
+- [script](https://github.com/grandatlant/DataMod/blob/main/pack_patch_mpq.py) for packing all files from Creature/, Fonts/, Sound/ to one 'patch-Y.MPQ'
 - [script](https://github.com/Soulsbane/WowRemoveUnusedSV/blob/master/remove-unused-sv.py) for SavedVariables cleanup in your WTF/ (stolen from https://github.com/Soulsbane/WowRemoveUnusedSV with WoW path override to "../")
 
--- pack_patch_mpq.py
+++ pack_patch_mpq.py
@@ -11,46 +11,57 @@ https://github.com/bubio/smpq
 https://www.zezula.net/en/mpq/download.html#StormLib
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.3'
 __copyright__ = 'Copyright (C) 2025 grandatlant'
 
-# Global defaults
-patch_name = 'patch-Y.MPQ'
-patch_content = [
-    'Creature',
-    'Fonts',
-    'Sound',
-]
-
 import os
+import sys
 import subprocess
 from argparse import ArgumentParser
 
 import logging
 logging.basicConfig(
     level = logging.DEBUG if __debug__ else logging.ERROR,
+    stream = sys.stdout,
     style = '{',
     format = '{levelname}::{message}'
 )
 log = logging.getLogger(__name__)
 
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    def load_dotenv(*a, **k):
+        log.warning('load_dotenv() call with missing python-dotenv.')
+load_dotenv()
+
+# Global defaults
+PATCH_NAME = 'patch-Y.MPQ'
+PATCH_CONTENT = [
+    'Creature',
+    'Fonts',
+    'Sound',
+]
 # I do not support Windows users, but lets give it a shot.
 # You can override it if you want to change SMPQ system command prefix
 # or use another utility
-smpq_cmd = ['smpq'] if os.name == 'posix' else [r'C:\smpq\build\smpq.exe']
+SMPQ_CMD = 'smpq' if os.name == 'posix' else r'C:\smpq\build\smpq.exe'
+smpq = os.getenv('SMPQ', SMPQ_CMD)
 
 
-def init_patch(name: str):
+def init_patch(name: str, mpq_version: str = '2') -> int:
+    """Creates new MPQ file witn given "name" and "version" using smpq util.
+    Return value: status code returned by smpq."""
     command = [
-        *smpq_cmd,
+        smpq,
         '--create',
         '--mpq-version',
-        '2',
+        str(mpq_version),
         '--force',
         '--verbose',
-        '"%s"' % name if ' ' in name else name,
+        name,
     ]
-    print(f'Patch "%s", init: ' % name, *command)
+    log.info('Patch "%s", init: %s', name, command)
     result = subprocess.run(
         command,
         text=True,
@@ -58,22 +69,26 @@ def init_patch(name: str):
         capture_output=True,
     )
     if result.stdout:
-        print(f'Init STDOUT:\n{result.stdout}')
+        log.info('Output:\n%s', result.stdout)
     if result.stderr:
-        print(f'Init STDERR:\n{result.stderr}')
-    print(f'Patch "%s" init finished. Return code:' % name, result.returncode)
+        log.info('Errors:\n%s', result.stderr)
+    log.info('Patch "%s" init finished. Return code: %i',
+             name, result.returncode)
     return result.returncode or 0
 
 
 def append_files(patch: str, files: list) -> int:
+    """Append existing MPQ file "patch"
+    with list of "files" given using smpq util.
+    Return value: status code returned by smpq."""
     command = [
-        *smpq_cmd,
+        smpq,
         '--append',
         '--verbose',
         patch,
         *files,
     ]
-    print(f'Patch "%s", append files.' % patch)
+    log.info('Patch "%s", append files.', patch)
     result = subprocess.run(
         command,
         text=True,
@@ -81,39 +96,45 @@ def append_files(patch: str, files: list) -> int:
         capture_output=True,
     )
     if result.stdout:
-        print(f'Append STDOUT:\n{result.stdout}')
+        log.info('Output:\n%s', result.stdout)
     if result.stderr:
-        print(f'Append STDERR:\n{result.stderr}')
-    print(f'Patch "%s" append finished. Return code:' % patch, result.returncode)
+        log.info('Errors:\n%s', result.stderr)
+    log.info('Patch "%s" append finished. Return code: %i',
+             patch, result.returncode)
     return result.returncode or 0
 
 
 def list_dir_files(dirname: str) -> list:
-    files = []
+    """os.walk directory named "dirname"
+    and form a list of all files in it 
+    with relative dirpath included to each filename.
+    Return value: list of strings representing filenames."""
+    files = list()
     for dirpath, dirnames, filenames in os.walk(dirname):
         for filename in filenames:
-            name = os.path.join(dirpath, filename)
-            files.append('"%s"' % name if ' ' in name else name)
+            files.append(os.path.join(dirpath, filename))
     return files
 
 
 def append_patch(name: str, content: list) -> int:
-
+    """Append files listed in "content"
+    to MPQ archive named "name".
+    Return value: int - combination (bitwise OR) of return codes,
+    returned by smpq utility while executing append_files() calls."""
     # status code gathering variable
     append_result = int()
     for item in content:
         log.debug('Processing content item "%s".', item)
         if os.path.isfile(item):
-            print(f'Append file "{item}".')
+            log.info('Append file "%s".', item)
             append_result |= append_files(name, [item])
         elif os.path.isdir(item):
-            print(f'Append ALL files from dir "{item}" (recursive).')
+            log.info('Append files from dir "%s" (recursive).', item)
             append_result |= append_files(name, list_dir_files(item))
         else:
             log.warning('Item "%s" is not file nor directory. Skip.', item)
     else:
         log.debug('Content processing finished in full. Content: %s', content)
-    
     return append_result
 
 
@@ -124,7 +145,6 @@ def parse_cli_args(args=None):
         allow_abbrev = False,
         epilog = __copyright__,
     )
-    
     parser.add_argument(
         '-v',
         '--version',
@@ -132,44 +152,45 @@ def parse_cli_args(args=None):
         version = f'%(prog)s {__version__}',
     )
     parser.add_argument(
-        '-p',
-        '--patch',
-        default = patch_name,
-        help = f'''name for new MPQ archive. 
-        Default filename "{patch_name}" script-defined.''',
-    )
-    parser.add_argument(
         '-a',
         '--append',
         action = 'store_true',
         default = False,
-        help = f'''append existing patch file.
-        New force-created by default.''',
+        help = '''append existing patch file. 
+        New force-created by default or if not exists.''',
+    )
+    parser.add_argument(
+        '-p',
+        '--patch',
+        default = PATCH_NAME,
+        help = f'''MPQ archive name. 
+        Default filename "{PATCH_NAME}" script-defined.''',
     )
     parser.add_argument(
         'content',
         nargs = '*',
-        default = patch_content,
+        default = PATCH_CONTENT,
         help = f'''name of directories or files to pack.
-        Default content "{patch_content}" script-defined.''',
+        Default content "{PATCH_CONTENT}" script-defined.''',
     )
-    
-    return parser.parse_args()
+    return parser.parse_args(args)
 
 
 ##  MAIN ENTRY POINT
 def main(args=None):
-    args = parse_cli_args(args)
-    log.debug('Parsed args: {args}'.format(args=args))
+    if args is sys.argv:
+        args = args[1:]
+    parsed = parse_cli_args(args)
+    log.debug('Parsed args: %s', parsed)
     
-    if args.content:
-        if not args.append or not os.path.exists(args.patch):
+    if parsed and parsed.content:
+        if not parsed.append or not os.path.exists(parsed.patch):
             # init new file first
-            if init_result := init_patch(args.patch):
+            if init_result := init_patch(parsed.patch):
                 log.error('init_patch() error. Status code: %i.', init_result)
                 return init_result
-        
-        if append_result := append_patch(args.patch, args.content):
+        # now append existing patch
+        if append_result := append_patch(parsed.patch, parsed.content):
             log.error('append_files() error. Status code: %i.', append_result)
             return append_result
     
@@ -178,4 +199,4 @@ def main(args=None):
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main(sys.argv))
